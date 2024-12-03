cc502f1a84b9c8ebdc7b9dfa6d0efa79c0d92085
f5ml7aj <tassoutassoujerbya@gmail.com>
2024-12-03 10:17:01 -0800

    Update imvu_bot.py

--- imvu_bot.py
+++ imvu_bot.py
@@ -12,6 +12,7 @@ import random
 
 # إعداد متصفح Chrome
 chrome_options = Options()
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-extensions")
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--no-sandbox")
